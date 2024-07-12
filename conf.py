0f6b1290f4d4a7436a6bbf3d3ccac02de6980ede
Adam Compton <comptona@gmail.com>
2024-07-11 23:24:50 -0700

    tweaking the title, adding the logo display

--- conf.py
+++ conf.py
@@ -17,10 +17,10 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Adam Compton"  # (translatable)
-BLOG_TITLE = "Kairos and Praxis"  # (translatable)
+BLOG_TITLE = "Adam Compton"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "https://handyman5.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
